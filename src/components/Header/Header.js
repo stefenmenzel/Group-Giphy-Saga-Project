880e3c4334123b8adb02e31928862eaf64ee53dc
mudman28 <johnnie.walters@gmail.com>
2019-05-16 13:51:41 -0500

    header/results

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-
+import './Header.css'
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 class Header extends Component {
 
     render() {
-      return (
-        <div>
-          <h1>Giphy Search!</h1>
-        </div>
+        return (
+            <div>
+                <h1>Find Gifs</h1>
+                <Router>
+                    <div className ="pageLinks">
+                        <ul>
+                            <li>
+                                <Link to="/">Search</Link>
+                            </li>
+                            <li>
+                                <Link to="/favorites">Favorite</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </Router>
+            </div>
         );
     }
         
