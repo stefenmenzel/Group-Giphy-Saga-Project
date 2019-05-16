ae5697ab9ecc097a24b63fbb6ade2784eedb049a
Madeline Morton <madelinemorton614@gmail.com>
2019-05-16 11:58:18 -0500

    database notes

--- database.sql
+++ database.sql
@@ -6,9 +6,33 @@ CREATE DATABASE "giphy_search_favorites";
 -- Category table
 CREATE TABLE "category" (
     "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL
+    "category_name" VARCHAR (120) NOT NULL
 );
 
--- Default categories. You may change them :)
-INSERT INTO "category" ("name")
-VALUES ('funny'), ('vega'), ('cartoon'), ('nsfw'), ('meme');
+-- Favorite Link Table
+CREATE TABLE "favorite_links" (
+	"id" SERIAL PRIMARY KEY,
+	"giphy_link" VARCHAR (250) NOT NULL
+);
+
+
+-- Favorite_Link_Category Table
+CREATE TABLE "favorites_link_category" (
+	"id" SERIAL PRIMARY KEY,
+	"cat_id" VARCHAR,
+	"link_id" VARCHAR
+);
+
+
+-- Default categories. 
+INSERT INTO "category" ("category_name")
+VALUES ('Asshole_Cats'), ('Babies'), ('Ron_Swanson');
+
+-- Default gifs
+INSERT INTO "favorite_links" ("giphy_link")
+VALUES ('https://gph.is/1Izr2ao'), ('https://gph.is/2jIZynP'), ('https://gph.is/18UySK4');
+
+
+-- Default 'favorite_link_category' table values
+INSERT INTO "favorites_link_category" ("cat_id", "link_id")
+VALUES (1, 1), (2, 2), (3, 3);
