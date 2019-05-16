1cb95a2ab3fa5e86c654c8bf3d968ef739718229
mudman28 <johnnie.walters@gmail.com>
2019-05-16 13:08:43 -0500

    header complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 import Search from '../Search/Search.js';
 import Favorites from '../Favorites/Favorites.js';
-import Header from '../Favorites/Favorites.js';
+import Header from '../Header/Header.js';
 class App extends Component {
 
   render() {
