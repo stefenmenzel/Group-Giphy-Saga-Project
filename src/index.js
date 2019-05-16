23c50893200556e94aaeef531d075a1edc34d992
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-05-16 11:46:20 -0500

    got redux and saga setup and talking...still need to make all the reducers and axios saga functions but pushing now for posterity

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {logger} from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put as dispatch} from 'redux-saga/effects';
+import axios from 'axios';
+
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+const sagaMiddleware = createSagaMiddleware();
+
+function* watcherSaga(){
+    console.log('saga is always watching');
+}
+
+const searchReducer = (state = [], action) => {
+    if(action.type === 'SET_SEARCH'){
+        return action.payload;
+    }
+    return state;
+}
+
+const store = createStore(
+    combineReducers({
+        searchReducer,
+    }),
+    applyMiddleware(logger, sagaMiddleware)
+);
+
+sagaMiddleware.run(watcherSaga);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
